5281c1df0340e22a32eef759f3cc4238c71db36f
Yasin Wanyonyi <yasinwanyonyi06@gmail.com>
2025-02-20 12:28:03 +0300

    working on menu

--- src/components/Home_page.vue
+++ src/components/Home_page.vue
@@ -46,19 +46,19 @@
 
                 <v-col cols="12" md="4">
                     <v-card>
-                    <v-card color="yellow">
+                    <v-card color="brown">
                     <v-card-item>
                     <v-card-title>Upcoming Events</v-card-title>
 
-                    <v-card-subtitle>This is a card subtitle</v-card-subtitle>
+                    <v-card-subtitle>Kareoke Fridays!!</v-card-subtitle>
                     </v-card-item>
-                    <v-img height="200px" src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg" cover ></v-img>
+                    <v-img height="200px" src="/igor-rodrigues-HyrmuhC3G3E-unsplash.jpg" cover ></v-img>
 
                     <v-card-text>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, ratione debitis quis est labore voluptatibus!
+                        Karaoke is the ultimate chance to unleash your inner superstar! Grab a mic, pick your favorite song, and belt it out like nobody's watching (even if they are).
                     </v-card-text>
                     </v-card>
-                    <v-card color="yellow">
+                    <v-card color="Cyan">
                     <v-card-item>
                     <v-card-title>Upcoming Events</v-card-title>
 
@@ -201,6 +201,47 @@
                 </v-col>
             </v-row>
         </v-container>
+        </v-card>
+    </v-container>
+    //Mid section
+    <v-container>
+        <v-card class="mx-auto" elevation="24" color = "purple">
+            <v-container>
+                <v-row >
+                    <v-col cols="12" md="4">
+                        <v-card>
+                            <v-card-item>
+                                <v-card-title>Contact Us</v-card-title>
+                            </v-card-item>
+                            <v-card-text><v-icon icon="mdi-phone" /> 0722123456</v-card-text>
+                            <v-card-text><v-icon icon="mdi-email" /> myrestaurant@example.com</v-card-text>
+                            <v-card-text>Operating Hours: 10.00am - 10.00pm</v-card-text>
+                        </v-card>
+                    </v-col>
+
+                        <v-col cols="12" md="4">
+                        <v-card>
+                            <v-card-item>
+                            <v-card-title>Find us on our socials</v-card-title>
+
+                            </v-card-item>
+                            <v-card-text><v-icon icon="mdi-instagram" /> myrestaurant</v-card-text>
+                            <v-card-text><v-icon icon="mdi-twitter" /> myrestaurant</v-card-text>
+                            <v-card-text><v-icon icon="mdi-facebook" /> myrestaurant</v-card-text>
+                        </v-card>
+                    </v-col>
+                    <v-col cols="12" md="4">
+                        <v-card>
+                            <v-card-item>
+                            <v-card-title>Our Restaurant Locations</v-card-title>
+                            </v-card-item>
+                            <v-card-text>Madaraka</v-card-text>
+                            <v-card-text>CBD</v-card-text>
+                            <v-card-text>Langata</v-card-text>
+                        </v-card>
+                    </v-col>
+                </v-row>
+            </v-container>
         </v-card>
     </v-container>
 
